0d28d7757e42ff34eeda9aab6e2aac0df6591583
Vlad Dunca <vladdnk18@gmail.com>
2024-11-07 16:27:48 +0100

    Reinitialized repo

--- src/main/java/org/example/SeleniumService.java
+++ src/main/java/org/example/SeleniumService.java
@@ -34,7 +34,7 @@ public class SeleniumService {
     }
 
     public String runAutomation(String candidateId) {
-        System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver-win64/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Chrome Driver\\chromedriver-win64");
 
         ChromeOptions options = getChromeOptions();
 
